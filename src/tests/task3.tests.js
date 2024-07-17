e53238f3628bff1265de7db7cccb331bdca6274d
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2024-07-17 12:29:53 +0400

    test environments are integrated

--- src/tests/task3.tests.js
+++ src/tests/task3.tests.js
@@ -101,6 +101,14 @@ describe("Test Google Cloud Page", () => {
 
         // ENV variables
         const varNumInstances = browser.testData.numInstances;
+        const varOperatingSystem = browser.testData.operatingSystem;
+        const varProvisionModel = browser.testData.provisionModel;
+        const varMachineType = browser.testData.machineType;
+        const varCpuModel = browser.testData.cpuModel;
+        const varNumCPUs = browser.testData.numCPUs;
+        const varLocalSSD = browser.testData.localSSD;
+        const varRegion = browser.testData.region;
+        const varCommitYear = browser.testData.commitYear;
 
         // Number of Instances - 4
         const numberOfInstance = await page("outcome").numberOfInstance.getText();
@@ -108,37 +116,37 @@ describe("Test Google Cloud Page", () => {
 
         // Operating System / Software: Free: Debian, CentOS, CoreOS, Ubuntu, or another User-Provided OS
         const operatingSystem = await page("outcome").operatingSystem.getText();
-        expect(operatingSystem).toContain("Free: Debian, CentOS, CoreOS, Ubuntu or BYOL (Bring Your Own License)");
+        expect(operatingSystem).toContain(varOperatingSystem);
 
         // Provisioning model: Regular
         const provisionModel = await page("outcome").provisionModel.getText();
-        expect(provisionModel).toHaveText("Regular");
+        expect(provisionModel).toHaveText(varProvisionModel);
 
         // Machine Family: General purpose
         // Series: N1
 
         // Machine type: n1-standard-8 (vCPUs: 8, RAM: 30 GB)
         const machineTypeFinal = await page("outcome").machineTypeFinal.getText();
-        expect(machineTypeFinal).toContain("n1-standard-8, vCPUs: 8, RAM: 30 GB");
+        expect(machineTypeFinal).toContain(varMachineType);
 
         // GPU type: NVIDIA Tesla V100
         const cpuModel = await page("outcome").cpuModel.getText();
-        expect(cpuModel).toContain("NVIDIA TESLA");
+        expect(cpuModel).toContain(varCpuModel);
 
         // Number of GPUs: 1
         const numberOfCPU = await page("outcome").numberOfCPU.getText();
-        expect(numberOfCPU).toBe("1");
+        expect(numberOfCPU).toBe(varNumCPUs);
 
         // Local SSD: 2x375 Gb
         const localSSD = await page("outcome").localSSD.getText();
-        expect(localSSD).toContain("2x375");
+        expect(localSSD).toContain(varLocalSSD);
 
         // Datacenter location: Frankfurt (europe-west3)
         const region = await page("outcome").region.getText();
-        expect(region).toContain("europe-west");
+        expect(region).toContain(varRegion);
 
         // Committed usage: 1 Year
         const commitYear = await page("outcome").commitYear.getText();
-        expect(commitYear).toContain("1");
+        expect(commitYear).toContain(varCommitYear);
     });
 });
